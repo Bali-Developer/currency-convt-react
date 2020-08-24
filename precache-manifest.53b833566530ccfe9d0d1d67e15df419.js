a4c42469d750db46acb21695729132d68ff34de5
bali <you@example.com>
2020-08-24 01:38:02 -0400

    Updates

---
+++ precache-manifest.53b833566530ccfe9d0d1d67e15df419.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d751b2e00e0f51648c1228537a2c55c7",
+    "url": "/currency-convt-react/index.html"
+  },
+  {
+    "revision": "cc6d282148721098b285",
+    "url": "/currency-convt-react/static/css/main.44f89b16.chunk.css"
+  },
+  {
+    "revision": "40bd33cf375a93ef3ac6",
+    "url": "/currency-convt-react/static/js/2.48a2b340.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currency-convt-react/static/js/2.48a2b340.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc6d282148721098b285",
+    "url": "/currency-convt-react/static/js/main.ca199d3e.chunk.js"
+  },
+  {
+    "revision": "1e75f30f25baf7357711",
+    "url": "/currency-convt-react/static/js/runtime-main.94678d08.js"
+  }
+]);
